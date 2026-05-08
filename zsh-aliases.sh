f131a43b3f0cab66b2660093e0fcbd264df2b92a
Jeremy Graeme <jeremy.graeme@hey.com>
2026-05-08 09:55:11 -0700

    Shorter aliases plz

--- zsh-aliases.sh
+++ zsh-aliases.sh
@@ -3,10 +3,10 @@
 #   source ~/.config/worklogs/aliases.zsh
 
 # Log work on a Jira ticket
-alias log-work='~/.local/bin/log-work.sh'
+alias log='~/.local/bin/log-work.sh'
 
 # Gather all worklogs into today's daily log
-alias gather-daily='~/.local/bin/gather-daily.sh'
+alias daily='~/.local/bin/gather-daily.sh'
 
 # Add a free-form note to a daily log
-alias add-to-daily='~/.local/bin/add-to-daily.sh'
+alias note='~/.local/bin/add-to-daily.sh'
